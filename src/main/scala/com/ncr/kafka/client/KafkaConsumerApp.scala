fd8808f9c41fbd8248d3f72597afaf62618c65b0
kehangchen <kehangchem@yahoo.com>
2019-12-20 10:22:25 -0500

    modified to have correct names and consumer to read properties from a specified file

--- src/main/scala/com/ncr/kafka/client/KafkaConsumerApp.scala
+++ src/main/scala/com/ncr/kafka/client/KafkaConsumerApp.scala
@@ -1,10 +1,33 @@
 package com.ncr.kafka.client
 
+import java.io.{FileInputStream, FileNotFoundException}
+import java.nio.file.Files
 import java.util.{Collections, Properties}
 import java.util.regex.Pattern
+
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.slf4j.LoggerFactory
+
 import scala.collection.JavaConverters._
 object KafkaConsumerApp extends App {
+
+  lazy val logger = LoggerFactory.getLogger(getClass)
+
+  val path = "/Volumes/Macintosh HD2/Users/kehangchen/Documents/mes/scala/kafka-client/consumer.properties"
+  val in = new FileInputStream(path)
+  val kafka_props: Properties = new Properties()
+  if (in != null) {
+    try {
+      kafka_props.load(in)
+    }
+    finally {
+      in.close()
+    }
+  }
+  else {
+    logger.error("properties file cannot be loaded at path " + path)
+    throw new FileNotFoundException("Properties file cannot be loaded")
+  }
 
   val props:Properties = new Properties()
   props.put("group.id", "test")
--- src/main/scala/com/ncr/kafka/client/KafkaProducerApp.scala
+++ src/main/scala/com/ncr/kafka/client/KafkaProducerApp.scala
@@ -11,19 +11,19 @@ object KafkaProducerApp extends App {
   lazy val logger = LoggerFactory.getLogger(getClass)
 
   val url = getClass.getResource("application.properties")
-  val ext_props: Properties = new Properties()
+  val kafka_props: Properties = new Properties()
 
   if (url != null) {
     val source = Source.fromURL(url)
-    ext_props.load(source.bufferedReader())
+    kafka_props.load(source.bufferedReader())
   }
   else {
     logger.error("properties file cannot be loaded at path " + url)
     throw new FileNotFoundException("Properties file cannot be loaded")
   }
 
-  val producer = new KafkaProducer[String, String](ext_props)
-  val topic = ext_props.getProperty("topic_name")
+  val producer = new KafkaProducer[String, String](kafka_props)
+  val topic = kafka_props.getProperty("topic_name")
   try {
     for (i <- 0 to 15) {
       val record = new ProducerRecord[String, String](topic, i.toString, "This is message we put into Kafka " + i)
