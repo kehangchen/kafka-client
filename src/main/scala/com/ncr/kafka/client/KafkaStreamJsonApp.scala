ab7827cc18ccd41a3a6623339bd5589e9153e4a1
kehangchen <kehangchem@yahoo.com>
2020-02-03 20:31:53 -0500

    this commit still works but its new outStream is not useful

--- src/main/scala/com/ncr/kafka/client/KafkaStreamJsonApp.scala
+++ src/main/scala/com/ncr/kafka/client/KafkaStreamJsonApp.scala
@@ -19,11 +19,22 @@ object KafkaStreamJsonApp extends App {
 
   val config: Properties = {
     val p = new Properties()
-    p.put(StreamsConfig.APPLICATION_ID_CONFIG, "add-metadata-application")
+    p.put(StreamsConfig.APPLICATION_ID_CONFIG, "in-metadata-application")
     val bootstrapServers = if (args.length > 0) args(0) else "localhost:9092"
     p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
     p
   }
+
+  val outConfig: Properties = {
+    val p = new Properties()
+    p.put(StreamsConfig.APPLICATION_ID_CONFIG, "out-metadata-application")
+    val bootstrapServers = if (args.length > 0) args(0) else "localhost:9092"
+    p.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+    p
+  }
+
+  val outBuilder = new StreamsBuilder()
+  val outRecord: KStream[String, String] = outBuilder.stream[String, String]("streams-metadata-output")
 
   val builder = new StreamsBuilder()
   val payload: KStream[String, String] = builder.stream[String, String]("streams-metadata-input")
@@ -31,30 +42,28 @@ object KafkaStreamJsonApp extends App {
     .flatMapValues(textLine => {
       for {
         Some(M(map)) <- List(JSON.parseFull(textLine))
-        //L(metadata) = map("header")
+        M(metadata) = map("header")
         L(body) = map("Body")
-
       } yield {
-        body.foreach(println)
+        body.foreach(m => outRecord.mapValues(m => JSONObject(m.asInstanceOf[Map[String, Any]]).toString()).to("streams-metadata-output"))
         body
       }
     })
 
+  val outStreams: KafkaStreams = new KafkaStreams(outBuilder.build(), outConfig)
+  outStreams.cleanUp()
+  outStreams.start()
   //jsonArray.to("streams-metadata-output")
   val streams: KafkaStreams = new KafkaStreams(builder.build(), config)
-
   streams.cleanUp()
-
   streams.start()
 
   // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
   sys.ShutdownHookThread {
+    outStreams.close(Duration.ofSeconds(1))
     streams.close(Duration.ofSeconds(1))
   }
-  /*
-  val jsonString: String = payload
-    .flatMapValues(textLine => textLine.toString).toString
-*/
+
   class CC[T] { def unapply(a:Any):Option[T] = Some(a.asInstanceOf[T]) }
 
   object M extends CC[Map[String, Any]]
