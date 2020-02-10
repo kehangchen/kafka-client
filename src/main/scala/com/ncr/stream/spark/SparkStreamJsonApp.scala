e905867d0fb4dd8b89121d9ade5191e8ce11a641
kehangchen <kehangchem@yahoo.com>
2020-02-09 21:26:05 -0500

    its result is the same as kafk-client now

--- src/main/scala/com/ncr/stream/spark/SparkStreamJsonApp.scala
+++ src/main/scala/com/ncr/stream/spark/SparkStreamJsonApp.scala
@@ -3,43 +3,47 @@ package com.ncr.stream.spark
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
+import net.liftweb.json._
 
-object SparkStreamJsonApp {
-
-  def main(arg: Array[String]) {
+object SparkStreamJsonApp extends App {
 
     var logger = Logger.getLogger(this.getClass())
 
-    val jobName = "MainExample"
+    val jobName = "stream-kafka-topics"
 
     val conf = new SparkConf().setAppName(jobName)
     val sc = new SparkContext(conf)
 
-    val spark = SparkSession.builder.
-      master("local")
-      .appName("spark session example")
+    val spark = SparkSession.builder
+      .master("local")
+      .appName("spark-stream-application")
       .getOrCreate()
 
     import spark.implicits._
-    val lines = spark
+    val payload = spark
       .readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "test")
+      .option("subscribe", "spark-streams-input")
       .load()
       .selectExpr("CAST(value AS STRING)")
       .as[String]
 
-    // Generate running word count
-    val wordCounts = lines.flatMap(_.split(" ")).groupBy("value").count()
+    val events = payload.flatMap(textLine => {
+      val raw = JsonParser.parse(textLine)
+      val metadata = raw \\ "header"
+      val JObject(body) = (raw \\ "Body")
+      val JArray(a) = body(0).value
+      ((a map (_ merge metadata)) map (JsonAST.compactRender(_).replace("header", "metadata"))).toArray.mkString("\n").split("\\n")
+    })
 
     // Start running the query that prints the running counts to the console
-    val query = wordCounts.writeStream
-      .outputMode("complete")
-      .format("console")
+    val query = events.writeStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("topic", "spark-streams-output")
+      .option("checkpointLocation", "/Users/kehangchen/Documents/rcg/ncr/spark-client")
       .start()
 
     query.awaitTermination()
-
-  }
 }
